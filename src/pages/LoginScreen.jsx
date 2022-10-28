425c73e4d3f3aa18d72833d38a367e4a03e31344
Erickbut <erickbutron1@gmail.com>
2022-10-28 18:43:04 -0400

    Third commit

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -9,34 +9,32 @@ const LoginScreen = () => {
   const [isLogged, setIsLogged] = useState(false)
 
   const submit = data => {
-    const URL ='https://ecommerce-api-react.herokuapp.com/api/v1/users/login'
-    axios.post(URL,data)
-    .then(res => {
-      console.log(res.data)
-      localStorage.setItem('token', res.data.data.token)
-      setIsLogged(true)
-    })
-    .catch(err => console.log(err))
+    const URL = 'https://ecommerce-api-react.herokuapp.com/api/v1/users/login'
+    axios.post(URL, data)
+      .then(res => {
+        console.log(res.data)
+        localStorage.setItem('token', res.data.data.token)
+        setIsLogged(true)
+      })
+      .catch(err => console.log(err))
   }
 
   useEffect(() => {
-    if(localStorage.getItem('token'))
-    {setIsLogged(true)}
-    else{setIsLogged(false)}
+    if (localStorage.getItem('token')) { setIsLogged(true) }
+    else { setIsLogged(false) }
   }, [])
-  
-  const handleLogout =()=>{
+
+  const handleLogout = () => {
     localStorage.removeItem('token')
     setIsLogged(false)
   }
-    
-  if(isLogged){
-return(
-  <div>
-    <h2>User Logged ✅</h2>
-    <button onClick={handleLogout}>Log Out</button>
-  </div>
-  )
+  if (isLogged) {
+    return (
+      <div>
+        <h2>User Logged ✅</h2>
+        <button onClick={handleLogout}>Log Out</button>
+      </div>
+    )
   }
 
   return (
@@ -48,7 +46,7 @@ return(
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" id='password'  {...register('password')}/>
+          <input type="password" id='password'  {...register('password')} />
         </div>
         <button>Login</button>
       </form>
